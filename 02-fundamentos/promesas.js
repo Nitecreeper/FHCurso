d179ba8b9ca350c9fc5e1282afc66632ba7de5fa
Jorge Guepud <jlguepudb@unal.edu.co>
2023-02-26 13:12:14 -0500

    Aprendizaje 26/02/2023

---
+++ 02-fundamentos/promesas.js
@@ -0,0 +1,82 @@
+const empleados = [
+    {
+        id: 1,
+        nombre: 'Fernando'
+    },
+    {
+        id: 2,
+        nombre: 'Linda'
+    },
+    {
+        id: 3,
+        nombre: 'Karen'
+    },
+];
+const salarios = [
+    {
+        id: 1,
+        salario: 1000
+    },
+    {
+        id: 2,
+        salario: 1500
+    },
+];
+
+const getEmpleado = ( id ) => {
+
+    return new Promise( ( resolve, reject ) => {
+        
+        const empleado = empleados.find( e => e.id === id)?.nombre;
+
+        ( empleado )
+            ? resolve( empleado )
+            : reject( `No existe el empleado con id ${id}` )
+
+        /* 
+        Este es el mismo if de forma normal.
+        if (empleado){
+            resolve( empleado );
+        } else {
+            reject( `No existe el empleado con id ${id}` )
+        } */
+
+
+    } );
+    
+}
+
+const getSalario = ( id ) => {
+    return new Promise( (resolve, reject) => {
+
+        const salario = salarios.find( s => s.id === id)?.salario;
+
+        ( salario )
+            ? resolve( salario )
+            : reject(`No existe el salario con id ${id}`)
+    });
+}
+
+let idtest = 3; 
+
+
+/*  Prueba de las funciones por separado
+getEmpleado(idtest)
+    .then( empleado => console.log( empleado ))
+    .catch( err => console.log(err) );
+
+getSalario(idtest)
+    .then( salario => console.log( salario ))
+    .catch( error => console.log(error)) */
+
+//PROMESAS EN CADENA 
+
+let nombre;
+
+getEmpleado(idtest)
+    .then( empleado => {
+        nombre = empleado;
+        return getSalario(idtest)
+    })
+    .then( salario => console.log( 'El empleado ', nombre, 'tiene un salario de', salario ))
+    .catch( err => console.log(err))
---
+++ 03-bases-node/app.js
@@ -0,0 +1,12 @@
+console.clear();
+
+let base = 5;
+
+console.log('========================')
+console.log(`      Tabla del ${base}`)
+console.log('========================')
+
+
+for(let i = 1; i<11; i++){
+    console.log(`${base} x ${i} = ${base*i}` );
+}
